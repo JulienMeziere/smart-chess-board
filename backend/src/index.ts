40c1081a966600bda66cdba7d0ee09aec4384402
Julien MEZIERE <julien.meziere@fashionnova.com>
2023-11-18 20:05:05 +0100

    [Backend] Refactored the backend to work with an interface common with the extension + made it parse the JSON messages correctly

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,37 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import http from 'http';
-import WebSocketManager, { RawData, MessageEvent } from 'ws';
+import WebSocketManager, { RawData, MessageEvent, Data } from 'ws';
 import readline from 'readline';
+import { WebSocketMessageType } from '../../extension/app/src/commonTypes';
+
+function parseMessage(data: RawData | Data): WebSocketMessageType | null {
+  try {
+    return JSON.parse(data.toString());
+  } catch (error) {
+    console.error('Error while parsing message', error);
+  }
+  return null;
+}
 
 /* Chrome Headless */
 let chromeHeadless: WebSocketManager;
 
 function onChromeHeadlessMessage({ data }: MessageEvent) {
-  console.log(`[Chrome] received: ${data.toString()}`);
+  const message = parseMessage(data);
+  if (!message) return;
+
+  switch (message.type) {
+    case 'OPPONENT_MOVE':
+      console.log('Opponent move:', message.data);
+      break;
+    case 'MOVE_STATUS':
+      console.log('Move status:', message.data);
+      break;
+    default:
+      console.warn('[Chrome headless message] Wrong message:', message);
+      break;
+  }
 }
 
 const app = express();
@@ -18,10 +41,10 @@ const port = 3000;
 
 app.use(cors());
 
-app.get('/test', (_: Request, res: Response) => {
-  console.log('Je recois une requete');
-  res.status(200).send('Hello world!');
-});
+// app.get('/test', (_: Request, res: Response) => {
+//   console.log('Je recois une requete');
+//   res.status(200).send('Hello world!');
+// });
 
 server.on('upgrade', (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
@@ -33,11 +56,21 @@ wss.on('connection', (ws: WebSocketManager) => {
   console.log('WebSocket connection established');
 
   ws.on('message', (data: RawData) => {
-    const message = data.toString();
-    if (message === '{ role: "chrome headless" }' && (chromeHeadless === undefined || chromeHeadless.readyState === WebSocketManager.CLOSED)) {
-      console.log('Chrome Headless connected');
-      chromeHeadless = ws;
-      chromeHeadless.addEventListener('message', onChromeHeadlessMessage);
+    const message = parseMessage(data);
+    if (!message || (ws === chromeHeadless && chromeHeadless.readyState === WebSocketManager.OPEN)) {
+      return;
+    }
+
+    switch (message.type) {
+      case 'ROLE':
+        if (message.data === 'chrome headless' && (!chromeHeadless || chromeHeadless.readyState !== WebSocketManager.OPEN)) {
+          console.log('Chrome Headless connected');
+          chromeHeadless = ws;
+          chromeHeadless.addEventListener('message', onChromeHeadlessMessage);
+        }
+        break;
+      default:
+        console.warn('Wrong message:', message);
     }
   });
 
